01fb636e73ff21b0622bf3b03e6b3dd67fd0be12
Louzet <micklouzet@hotmail.fr>
2020-05-11 23:39:36 +0200

    install tailwind-django, configure templates et start script fix

--- handleschool/settings.py
+++ handleschool/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 
     'accounts',
     'home'
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TAILWIND_APP_NAME = 'theme'
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
