02b707bbb530e60e0d4be3106989dea6e9c95ead
Louzet <micklouzet@hotmail.fr>
2020-05-12 17:06:24 +0200

    modif customUserAdmin model

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom profile",
+            {
+                "fields": (
+                    "avatar",
+                    "sex",
+                    "biography"
+                )
+            },
+        ),
+    )
